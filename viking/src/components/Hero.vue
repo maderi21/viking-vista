f88239e2d7d36b44b50b9a54f3ebf89cf678ba28
xkostolanskym <kostolansky.marek@gmail.com>
2024-06-06 21:35:14 +0200

    hero

--- viking/src/components/Hero.vue
+++ viking/src/components/Hero.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>This is Hero section</div>
+  <div class="bg-gray-300 w-full h-96 flex justify-center items-center">
+    <div class="flex items-center flex-col">
+      <h3>Your Adventure Begins Here</h3>
+      <p>Discover relax from different angle</p>
+    </div>
+  </div>
 </template>
 <script>
 export default {};
