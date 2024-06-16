06d08c36140d2cd292b4086f9a9606091ceb40dd
xkostolanskym <kostolansky.marek@gmail.com>
2024-06-16 20:44:21 +0200

    creating icon component

---
+++ viking/src/components/icons/Icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <font-awesome-icon :icon="icon" />
+</template>
+
+<script setup>
+import { defineProps, computed } from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fas);
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const icon = computed(() => {
+  return props.name;
+});
+</script>
