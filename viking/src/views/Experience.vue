abcc74e20016bef4463e1e305dfe405caf2c41e9
xkostolanskym <kostolansky.marek@gmail.com>
2024-06-26 09:31:27 +0200

    adding on hover classses

--- viking/src/views/Experience.vue
+++ viking/src/views/Experience.vue
@@ -3,23 +3,67 @@ defineProps({});
 </script>
 
 <template>
-  <div class="w-full mt-20 flex flex-col items-center justify-center">
+  <div class="w-full mt-20 mb-20 flex flex-col items-center justify-center">
     <div class="flex items-start">
-      <h3 class="text-4xl">What Does it Look Like?</h3>
+      <h3 class="text-4xl mb-10">What Does it Look Like?</h3>
     </div>
     <div class="grid grid-cols-4 gap-4">
-      <div>
-        <img src="../images/look1.jpg" alt="look1" />
+      <div class="h-[450px] w-[350px]">
+        <img
+          src="../images/look1.jpg"
+          alt="look1"
+          class="object-cover h-full w-full rounded-md shadow-md transform transition-transform duration-300 hover:scale-105"
+        />
       </div>
-      <div><img src="../images/look2.jpg" alt="look2" /></div>
-      <div>
-        <img src="../images/look3.jpg" alt="look3" />
+      <div class="h-[450px] w-[350px]">
+        <img
+          src="../images/look2.jpg"
+          alt="look2"
+          class="object-cover h-full w-full rounded-md shadow-md transform transition-transform duration-300 hover:scale-105"
+        />
       </div>
-      <div><img src="../images/look4.jpg" alt="look4" /></div>
-      <div><img src="../images/look5.jpg" alt="look5" /></div>
-      <div><img src="../images/look6.jpg" alt="look6" /></div>
-      <div><img src="../images/look7.jpg" alt="look7" /></div>
-      <div><img src="../images/look8.jpg" alt="look8" /></div>
+      <div class="h-[450px] w-[350px]">
+        <img
+          src="../images/look3.jpg"
+          alt="look3"
+          class="object-cover h-full w-full rounded-md shadow-md transform transition-transform duration-300 hover:scale-105"
+        />
+      </div>
+      <div class="h-[450px] w-[350px]">
+        <img
+          src="../images/look4.jpg"
+          alt="look4"
+          class="object-cover h-full w-full rounded-md shadow-md transform transition-transform duration-300 hover:scale-105"
+        />
+      </div>
+      <div class="h-[450px] w-[350px]">
+        <img
+          src="../images/look5.jpg"
+          alt="look5"
+          class="object-cover h-full w-full rounded-md shadow-md transform transition-transform duration-300 hover:scale-105"
+        />
+      </div>
+      <div class="h-[450px] w-[350px]">
+        <img
+          src="../images/look6.jpg"
+          alt="look6"
+          class="object-cover h-full w-full rounded-md shadow-md transform transition-transform duration-300 hover:scale-105"
+        />
+      </div>
+      <div class="h-[450px] w-[350px]">
+        <img
+          src="../images/look7.jpg"
+          alt="look7"
+          class="object-cover h-full w-full rounded-md shadow-md transform transition-transform duration-300 hover:scale-105"
+        />
+      </div>
+      <div class="h-[450px] w-[350px]">
+        <img
+          src="../images/look8.jpg"
+          alt="look8"
+          class="object-cover h-full w-full rounded-md shadow-md transform transition-transform duration-300 hover:scale-105"
+        />
+      </div>
     </div>
   </div>
 </template>
